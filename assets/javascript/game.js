bab6be583ee7ccd970533fd1f3a4ed7cd161def1
Tammy Richardson <atrich0220@gmail.com>
2020-02-23 14:17:44 -0700

    turned gem imgages into buttons that generate a random button that is displayed in the your total element

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,8 @@
-var blueGem = "";
-var purpleGem = "";
-var redGem = "";
-var yellowGem = "";
+
 var wins = 0;
 var losses = 0;
 var playerScore = 0;
-var oppScore = 0;
-var randomNumber = 0;
+
 var scoreStatus = 0;
 
 
@@ -17,13 +13,55 @@ $(document).ready(function () {
 
 
     //generates random number
-    var random = Math.floor(Math.random() * 150) + 50;
+    var computerNum = Math.floor(Math.random() * 100) + 1;
 
-    //displays random number in p1 on card
-    $("h5").text(random)
+    //displays random number in h5 on card
+    $("h5").text(computerNum)
+
 
   });
 
+  // assigns click to image on html
+  $("#purple").on("click", function () {
+
+    //generates random number for gem images
+    var purple = Math.floor(Math.random() * 10) + 1;
+
+    //displays random number in h4 on card
+    $("h4").text(purple)
+  });
+
 });
+
+// assigns click to image on html
+$("#blue").on("click", function () {
+
+  //generates random number for gem images
+  var blue = Math.floor(Math.random() * 10) + 1;
+
+  //displays random number in h4 on card
+  $("h4").text(blue)
+});
+
+// assigns click to image on html
+$("#red").on("click", function () {
+
+  //generates random number for gem images
+  var red = Math.floor(Math.random() * 10) + 1;
+
+  //displays random number in h4 on card
+  $("h4").text(red)
+});
+
+// assigns click to image on html
+$("#yellow").on("click", function () {
+
+  //generates random number for gem images
+  var yellow = Math.floor(Math.random() * 10) + 1;
+
+  //displays random number in h4 on card
+  $("h4").text(yellow)
+});
+
 
 
