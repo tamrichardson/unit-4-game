889413d4c22deb210f203896fc5a801f98096186
Tammy Richardson <atrich0220@gmail.com>
2020-02-24 10:44:37 -0700

    added functions to add gem totals, added reset function, added wins and losses tally function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,106 @@
-
-var wins = 0;
-var losses = 0;
-var playerScore = 0;
-
-var scoreStatus = 0;
-
-
 $(document).ready(function () {
 
   // assigns click to "a"
   $("a").on("click", function () {
-
 
     //generates random number
     var computerNum = Math.floor(Math.random() * 100) + 1;
 
     //displays random number in h5 on card
     $("h5").text(computerNum)
-
-
   });
 
-  // assigns click to image on html
-  $("#purple").on("click", function () {
 
-    //generates random number for gem images
-    var purple = Math.floor(Math.random() * 10) + 1;
-
-    //displays random number in h4 on card
-    $("h4").text(purple)
-  });
-
-});
-
-// assigns click to image on html
-$("#blue").on("click", function () {
-
-  //generates random number for gem images
+  //generates random numbers for gem images
+  var purple = Math.floor(Math.random() * 10) + 1;
   var blue = Math.floor(Math.random() * 10) + 1;
-
-  //displays random number in h4 on card
-  $("h4").text(blue)
-});
-
-// assigns click to image on html
-$("#red").on("click", function () {
-
-  //generates random number for gem images
   var red = Math.floor(Math.random() * 10) + 1;
-
-  //displays random number in h4 on card
-  $("h4").text(red)
-});
-
-// assigns click to image on html
-$("#yellow").on("click", function () {
-
-  //generates random number for gem images
   var yellow = Math.floor(Math.random() * 10) + 1;
 
-  //displays random number in h4 on card
-  $("h4").text(yellow)
+  var wins = 0;
+  var losses = 0;
+
+  var playerScore = 0;
+  $("playerScore").text(playerScore)
+
+  //adds numbers together and displays player score
+  $('#purple').click(() => {
+    playerScore = playerScore + purple;
+    $("h4").text(playerScore);
+    if (playerScore === computerNum) {
+      win();
+      reset();
+
+    } else if (playerScore > computerNum) {
+      lose();
+      reset();
+
+    }
+  });
+
+  $("#red").click(() => {
+    playerScore = playerScore + red;
+    $("h4").text(playerScore);
+    if (playerScore === computerNum) {
+      win();
+      reset();
+
+    } else if (playerScore > computerNum) {
+      lose();
+      reset();
+
+    }
+  });
+
+  $("#blue").click(() => {
+    playerScore = playerScore + blue;
+    $("h4").text(playerScore);
+    if (playerScore === computerNum) {
+      win();
+      reset();
+
+    } else if (playerScore > computerNum) {
+      lose();
+      reset();
+
+    }
+  });
+
+  $("yellow").click(() => {
+    playerScore = playerScore + yellow;
+    $("h4").text(playerScore);
+    if (playerScore === computerNum) {
+      win();
+      reset();
+
+    } else if (playerScore > computerNum) {
+      lose();
+      reset();
+
+    }
+  });
+
+  if (playerScore === computerNum) {
+    alert("You win!");
+  }
+  else if (playerscore >= computerNum) {
+    alert("You lose!");
+  }
+
+  function win() {
+    wins = wins = 1;
+    $("#wins").text(wins)
+    reset()
+  }
+
+  function lose() {
+    losses = losses = 1;
+    $("#losses").text(losses)
+    reset()
+  }
+
+  function reset() {
+    playerScore = 0;
+    $('#playerScore').text(playerScore);
+  }
 });
-
-
-
